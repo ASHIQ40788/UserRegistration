277532008ac5f84dd6e8f5a2e44424efafd67908
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-22 21:53:06 +0530

    [ASHIQ] added : UC3-Valid_Email

--- UserRegistrationAssignment.cs
+++ UserRegistrationAssignment.cs
@@ -10,9 +10,10 @@ namespace UserRegistration
     class UserRegistrationAssignment
     {
         //string pattern = "^[A-Z]{1}[a-z]{2,25}$"; //UC1-FirstName
-        string pattern = "^[A-Z]{1}[a-z]{2,30}[  ]*[A-Z]{1}[a-z]{2,20}$"; //UC2-FirstAndLastName
-                                   //writing the valid pattern
-        string[] inputs = { "Ashiq Shaik", "Ameer Shaik", "ashiq shaik", "ameer shaik", "Revanth Peddasingu", "Susank Behar", "susank behar" };//Validating some Example
+        //string pattern = "^[A-Z]{1}[a-z]{2,30}[  ]*[A-Z]{1}[a-z]{2,20}$"; //UC2-FirstAndLastName
+        string pattern = "^[a-zA-Z0-9]+[._+]{0,1}[a-zA-Z0-9]*@[a-zA-Z0-9]{1,15}.[a-zA-Z]{2,10}[.]*[a-zA-Z]*$";//UC3-E-mail.
+        //writing the valid pattern
+        string[] inputs = { "ashiqshaiksvali8", "ashiq.8shaik@gmail.com.au", "ashIqS?haik", "Ashiq788", "ashiqshaiksvali8@gmail.com", "SADNIJOQWN788BD", "ashiq.100@yahoo.com", "ashiq@1.com", "bridgelabz100+@gmail.com" };//Validating some Example
 
 
         /// <summary>
